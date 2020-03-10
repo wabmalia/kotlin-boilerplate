2c328b89cd26adbf8bb4cca71005a2f03895bb8c
Wilson Mália <wabmalia@gmail.com>
2020-03-10 19:49:34 +0000

    Add kotlin boilerplate with gradle

---
+++ build.gradle.kts
@@ -0,0 +1,26 @@
+plugins {
+    id("org.jetbrains.kotlin.jvm") version "1.3.61"
+
+    application
+}
+
+repositories {
+    jcenter()
+}
+
+dependencies {
+    implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    testImplementation("org.jetbrains.kotlin:kotlin-test")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit")
+}
+
+application {
+    mainClassName = "boilerplate.AppKt"
+}
+
+val jar by tasks.getting(Jar::class) {
+    manifest{
+        attributes["Main-Class"] = "boilerplate.AppKt"
+    }
+}
